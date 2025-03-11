e7f24b03f7337905c35910312fe6542900cb982c
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-03-11 15:19:04 +0100

    routing paths added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+import { ImpressumComponent } from './impressum/impressum/impressum.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy/privacy-policy.component';
+import { HelpComponent } from './help/help.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
-    // { path: 'imprint', component: ImprintComponent},
-    // { path: 'privacy-policy', component: PrivacyPolicyComponent}
+    { path: '', component: MainContentComponent},
+     { path: 'imprint', component: ImpressumComponent},
+     { path: 'privacy-policy', component: PrivacyPolicyComponent},
+     { path: 'help-page', component: HelpComponent}
 ];
