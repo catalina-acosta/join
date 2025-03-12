ca8c1bffc9ef7bf2e4813b979a366e5ed86d3982
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-03-12 13:28:12 +0100

    added contact component to main-content and removed it from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { FirebaseService } from './shared/service/firebase.service';
 import { SharedComponent } from "./shared/shared.component";
-import { ContactsComponent } from './main-content/contacts/contacts.component';
 import { AddContactDialogComponent } from "./main-content/contacts/add-contact-dialog/add-contact-dialog.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SharedComponent, ContactsComponent, AddContactDialogComponent, AddContactDialogComponent],
+  imports: [RouterOutlet, SharedComponent, AddContactDialogComponent, AddContactDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
