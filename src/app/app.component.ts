36a405980868c44a49d92b59d4261d50fc842ef7
Albina Januzi <albina_januzi@hotmail.com>
2025-03-12 10:16:22 +0100

    Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseService } from './shared/service/firebase.service';
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
