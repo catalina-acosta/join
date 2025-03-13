58b7dc6e639e40c5f24c37774b5e432f80715d50
Caryensong <caryensong@googlemail.com>
2025-03-13 13:45:21 +0100

    add icons

--- src/app/main-content/contacts/add-contact-dialog/add-contact-dialog.component.ts
+++ src/app/main-content/contacts/add-contact-dialog/add-contact-dialog.component.ts
@@ -5,40 +5,40 @@ import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact-dialog',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './add-contact-dialog.component.html',
   styleUrl: './add-contact-dialog.component.scss'
 })
 export class AddContactDialogComponent {
-  firebase =inject(FirebaseService);
+  firebase = inject(FirebaseService);
 
   @Output() closeDialogEvent = new EventEmitter<void>();
   formSubmitted = false;
   newContact = {
-    fullname:'',
-    firstname:'',
+    fullname: '',
+    firstname: '',
     lastname: '',
     email: '',
-    phone:'',
+    phone: '',
     color: '',
   }
-  
+
   onCreateContact(contactForm: NgForm) {
     this.formSubmitted = true;
     if (contactForm.valid) {
       this.addNewContact();
     }
   }
-  
+
   addNewContact() {
     const nameParts = this.newContact.fullname.trim().split(' ');
     this.newContact.firstname = this.toUpperCaseName(nameParts[0]);
     this.newContact.lastname = this.toUpperCaseName(nameParts.slice(1).join(' ') || '');
     this.newContact.color = this.firebase.avatarColor[Math.floor(Math.random() * this.firebase.avatarColor.length)]// color: this.firebase.service.avatarColor[Math.floor(Math.random() * this.avatarColor.length)]
 
-      this.firebase.addContactToData(this.newContact);
-      this.clearInputFeld();
+    this.firebase.addContactToData(this.newContact);
+    this.clearInputFeld();
   }
 
   toUpperCaseName(str: string): string {
@@ -46,19 +46,19 @@ export class AddContactDialogComponent {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
   }
 
-  clearInputFeld(){
+  clearInputFeld() {
     this.newContact = {
-    fullname: 'Name',
-    firstname: '',
-    lastname: '',
-    email: 'Email',
-    phone: 'Phone',
-    color:'',
-
+      fullname: 'Name',
+      firstname: '',
+      lastname: '',
+      email: 'Email',
+      phone: 'Phone',
+      color: '',
+    }
     this.closeDialog();
   }
-  
+
   closeDialog() {
-    this.closeDialogEvent.emit(); 
+    this.closeDialogEvent.emit();
   }
 }
