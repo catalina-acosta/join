2b712764cca5571e37b73534c41b5b1932fd20ef
Caryensong <caryensong@googlemail.com>
2025-03-13 15:29:07 +0100

    add delete-dialog style

---
+++ src/app/main-content/contacts/delete-dialog/delete-dialog.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-dialog',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './delete-dialog.component.html',
+  styleUrl: './delete-dialog.component.scss'
+})
+export class DeleteDialogComponent {
+
+}
