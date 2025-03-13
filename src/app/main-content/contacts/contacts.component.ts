5c00134d87c988f284355f71bab0b1c7de055c8c
Caryensong <caryensong@googlemail.com>
2025-03-13 15:29:07 +0100

    add delete-dialog style

--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [ContactsListComponent],
+  imports: [ContactsListComponent, DeleteDialogComponent],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
