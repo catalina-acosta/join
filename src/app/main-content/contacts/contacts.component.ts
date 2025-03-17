a7fbe760bba68067832e151edb3ac6c3f65c7891
Caryensong <caryensong@googlemail.com>
2025-03-17 12:05:08 +0100

    update name from firebase

--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -3,7 +3,6 @@ import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { CommonModule } from '@angular/common';
 import { ContactInterface } from './contact-interface';
-import { __makeTemplateObject } from 'tslib';
 import { FirebaseService } from '../../shared/service/firebase.service';
 
 @Component({
@@ -11,28 +10,15 @@ import { FirebaseService } from '../../shared/service/firebase.service';
   standalone: true,
   imports: [ContactsListComponent, ContactDetailsComponent, CommonModule],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent {
   firebaseService = inject(FirebaseService);
   detailsOpen: boolean = false;
-  @Input() contact!: ContactInterface; 
-  contactId?: string= '';
+  currentContact: ContactInterface | null = null;
 
-
-
-  detailsOpened(contact: ContactInterface | null) {
-    if (!contact) {
-      this.detailsOpen = false;
-      return;
-    }
-    this.contactId = contact.id;
-
-    if (this.contactId) {
-      this.firebaseService.deleteContactFromData(this.contactId);
-    }
-
+  detailsOpened(contact: ContactInterface) {
+    this.currentContact = contact;
     this.detailsOpen = true;
   }
-
 }
