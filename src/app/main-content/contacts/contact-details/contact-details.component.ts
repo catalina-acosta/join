0b66691933ee12b9e7620dfff4c1971248bac74c
Caryensong <caryensong@googlemail.com>
2025-03-17 11:53:46 +0100

    update

--- src/app/main-content/contacts/contact-details/contact-details.component.ts
+++ src/app/main-content/contacts/contact-details/contact-details.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FirebaseService } from '../../../shared/service/firebase.service';
+import { ContactInterface } from '../contact-interface';
+import { ContactsListComponent } from '../contacts-list/contacts-list.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-details',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule],
   templateUrl: './contact-details.component.html',
   styleUrl: './contact-details.component.scss'
 })
 export class ContactDetailsComponent {
+firebase = inject(FirebaseService);
+ @Input() contact!: ContactInterface; 
+ contactId?: string= '';
 
 }
--- src/app/main-content/contacts/contacts.component.ts
+++ src/app/main-content/contacts/contacts.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { CommonModule } from '@angular/common';
+import { ContactInterface } from './contact-interface';
+import { __makeTemplateObject } from 'tslib';
+import { FirebaseService } from '../../shared/service/firebase.service';
 
 @Component({
   selector: 'app-contacts',
-  imports: [ContactsListComponent],
+  standalone: true,
+  imports: [ContactsListComponent, ContactDetailsComponent, CommonModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
 export class ContactsComponent {
+  firebaseService = inject(FirebaseService);
+  detailsOpen: boolean = false;
+  @Input() contact!: ContactInterface; 
+  contactId?: string= '';
+
+
+
+  detailsOpened(contact: ContactInterface | null) {
+    if (!contact) {
+      this.detailsOpen = false;
+      return;
+    }
+    this.contactId = contact.id;
+
+    if (this.contactId) {
+      this.firebaseService.deleteContactFromData(this.contactId);
+    }
+
+    this.detailsOpen = true;
+  }
 
 }
