aa7e2f38d2e9a5c37682139def08d81cbf064d30
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-03-20 15:57:06 +0100

    added initial card styling to board.component

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -7,15 +7,28 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { TaskInterface } from './task.interface';
 
 @Component({
   selector: 'app-board',
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
   firebase = inject(FirebaseService);
 
-  todo = []
+  drop(event: CdkDragDrop<TaskInterface[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
