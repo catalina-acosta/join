6180974104a5fd8968b2e025b2f28acbcec740fa
BaraDroid <lambeinovab@seznam.cz>
2025-03-20 11:15:16 +0100

    forms module added

--- src/app/main-content/add-task/add-task.component.ts
+++ src/app/main-content/add-task/add-task.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
-
+  form = new FormControl('');
 }
