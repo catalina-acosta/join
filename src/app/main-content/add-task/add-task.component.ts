cd4c32904c7a158b072a90be1583126406a500f3
BaraDroid <lambeinovab@seznam.cz>
2025-03-21 11:17:01 +0100

    calender input field added directly to add-task.component

--- src/app/main-content/add-task/add-task.component.ts
+++ src/app/main-content/add-task/add-task.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalenderComponent } from './calender/calender.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 
 @Component({
   selector: 'app-add-task',
   imports: [FormsModule, CalenderComponent],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
-
+  todaysDate: string = new Date().toISOString().split('T')[0];
 }
