d1e2001cdea25264856b4fe7f9bdaea22ebcd1bc
Caryensong <caryensong@googlemail.com>
2025-03-21 09:15:18 +0100

    commit out task fo merge

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -13,15 +13,15 @@ import { CommonModule } from '@angular/common';
 })
 export class BoardComponent {
   firebase = inject(FirebaseService);
-  searchQuery: string = ''; 
-  tasks: TaskInterface[] = [
-    { id: '1', title: 'Einkaufen', description: 'Apfel', date: '2025-03-22', priority: 'High', userId: '123', category: 'User Story', subtask: '' },
-    { id: '2', title: 'Daily Stand UP', description: 'Project Besprechung', date: '2025-03-23', priority: 'Medium', userId: '456', category: 'Technical Task', subtask: '' },
-    { id: '3', title: 'Arbeiten', description: 'Join Projekt', date: '2025-03-24', priority: 'Low', userId: '789', category: '', subtask: '' }
-  ];
+   searchQuery: string = ''; 
+  // tasks: TaskInterface[] = [
+  //   { id: '1', title: 'Einkaufen', description: 'Apfel', date: '2025-03-22', priority: 'High', userId: '123', category: 'User Story', subtask: '' },
+  //   { id: '2', title: 'Daily Stand UP', description: 'Project Besprechung', date: '2025-03-23', priority: 'Medium', userId: '456', category: 'Technical Task', subtask: '' },
+  //   { id: '3', title: 'Arbeiten', description: 'Join Projekt', date: '2025-03-24', priority: 'Low', userId: '789', category: '', subtask: '' }
+  // ];
 
 
-  filteredTasks: TaskInterface[] = [...this.tasks]; 
+   filteredTasks: TaskInterface[] = [...this.tasks]; 
 
   filterTasks(): void {
     const query = this.searchQuery.toLowerCase().trim();
