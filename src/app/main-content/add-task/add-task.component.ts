3cb708a3d6be0fc5de0d3af48c056c8a36c53c0b
BaraDroid <lambeinovab@seznam.cz>
2025-03-21 16:03:45 +0100

    for loop for all contacts for selection added

--- src/app/main-content/add-task/add-task.component.ts
+++ src/app/main-content/add-task/add-task.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { FirebaseService } from '../../shared/service/firebase.service';
+import { ContactInterface } from '../contacts/contact-interface';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-add-task',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+  firebase = inject(FirebaseService);
+  currentContact: ContactInterface | null = null;
   todaysDate: string = new Date().toISOString().split('T')[0];
   selectedPriority: string = 'medium';
+  selectedContacts = [];  //dass ich das unten anzeigen kann
 
   selectPriority(priority: string) {
     this.selectedPriority = priority;
