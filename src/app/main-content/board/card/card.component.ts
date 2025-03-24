290033f909481598d82582e1110b05b6e84d5f92
Caryensong <caryensong@googlemail.com>
2025-03-24 14:32:26 +0100

    add edit-dialog style

--- src/app/main-content/board/card/card.component.ts
+++ src/app/main-content/board/card/card.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { TaskInterface } from '../task.interface';
 import { FirebaseService } from '../../../shared/service/firebase.service';
+import { EditContactDialogComponent } from '../../contacts/edit-contact-dialog/edit-contact-dialog.component';
+import { EditDialogComponent } from "../edit-dialog/edit-dialog.component";
 
 @Component({
   selector: 'app-card',
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule, EditDialogComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -15,7 +17,21 @@ export class CardComponent {
    @Output() closeDialogEvent = new EventEmitter<void>();
    @Input() item?: TaskInterface;
 
+   selectedItem?: TaskInterface;
+   isDialogOpen: boolean = false;
+
   closeDialog() {
     this.closeDialogEvent.emit();
+  }
+ 
+  openEditDialog(item: TaskInterface){
+    this.selectedItem = item;
+    this.isDialogOpen = true;
+    console.log(this.selectedItem);
+  }
+
+  
+  stopPropagation(event: Event) {
+    event.stopPropagation();
   }
 }
