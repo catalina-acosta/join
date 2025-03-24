07e8893aeda4bb01cdc3e0f737cfab56f66b6889
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-03-24 13:20:08 +0100

    board is fully styled and is also responsive up to 320px width

--- src/app/main-content/board/board.component.ts
+++ src/app/main-content/board/board.component.ts
@@ -95,11 +95,6 @@ export class BoardComponent {
       );
       const currentTask = event.container.data[event.currentIndex];
 
-      console.log("Task ID ", currentTask.id);
-      console.log("Old status ", currentTask.status);
-      console.log("New status ", event.container.id);
-      console.log("assigned users: ", currentTask.assignedToUserId)
-
       // Update the status of the task in the database (Parameter: taskId, newStatus)
       this.firebase.updateTaskStatus(currentTask.id!, event.container.id);
     }
