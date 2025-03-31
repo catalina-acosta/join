4493c1a6eb28e4302b5cf0854af5009e1a06a978
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-03-31 13:43:24 +0200

    drag preview and card rotation working

--- src/app/main-content/board/task-card/task-card.component.ts
+++ src/app/main-content/board/task-card/task-card.component.ts
@@ -12,12 +12,11 @@ import { CardComponent } from '../card/card.component';
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
-  @Input()
-  item!: TaskInterface; 
+  @Input() item!: TaskInterface; 
   firebase = inject(FirebaseService);
   completedSubtasks: number = 0;
   isDragging: boolean = false;
-  draggingCardId: string | null = null;
+  // draggingCardId: string | null = null;
   selectedItem!: TaskInterface;
   isDialogOpen: boolean = false;
 
@@ -32,26 +31,24 @@ export class TaskCardComponent {
 
   onDragStart(item:TaskInterface): void {
     console.log("dragging card");
-    
     if (item && item.id) {
       this.isDragging = true;
-      
-      this.draggingCardId = item.id; // Set the ID of the dragged card
-      console.log('Drag started for card ID:', item.id); // Debugging
+      // this.draggingCardId = item.id; // Set the ID of the dragged card
+      // console.log('Drag started for card ID:', item.id); // Debugging
     }
   }
 
   onDragEnd(item:TaskInterface): void {
     if (item && item.id) {
       this.isDragging = false;
-      this.draggingCardId = null; // Reset the dragged card ID
-      console.log('Drag ended for card ID:', item.id); // Debugging
+      // this.draggingCardId = null; // Reset the dragged card ID
+      // console.log('Drag ended for card ID:', item.id); // Debugging
     }
   }
 
-  isCardDragging(cardId: string): boolean {
-  return this.draggingCardId === cardId;
-  }
+  // isCardDragging(cardId: string): boolean {
+  // return this.draggingCardId === cardId;
+  // }
 
   openCardDialog(item: TaskInterface) {
     this.selectedItem = item;
