d9b8abc83609d4ce4ccc7df0857c013d00e18240
BaraDroid <lambeinovab@seznam.cz>
2025-04-01 14:34:49 +0200

    method for different greetings in summary

--- src/app/main-content/summary/summary.component.ts
+++ src/app/main-content/summary/summary.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
+guestLogIn: boolean = false;
+nowDate = new Date;
+currentHour = this.nowDate.getHours();
+
+
+//get different greetings for different day times
+getGreeting() {
+  if(this.currentHour > 3 && this.currentHour < 11) {
+    return 'Good morning';
+  }
+  else if(this.currentHour > 12 && this.currentHour < 16){
+    return 'Good afternoon';
+  }
+  else if(this.currentHour > 17 && this.currentHour < 20){
+    return 'Good evening';
+  }
+  else return 'Good night';
+}
+
+
 
 }
