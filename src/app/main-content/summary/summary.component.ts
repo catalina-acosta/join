1460efa7eded52fe7fac0f0a679f0ba05073a14c
BaraDroid <lambeinovab@seznam.cz>
2025-04-02 14:02:37 +0200

    routes to board component activated

--- src/app/main-content/summary/summary.component.ts
+++ src/app/main-content/summary/summary.component.ts
@@ -1,154 +1,156 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FirebaseService } from '../../shared/service/firebase.service';
 import { TaskInterface } from '../board/task.interface';
 import { collectionData, Firestore, collection } from '@angular/fire/firestore';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
 export class SummaryComponent {
-firebase = inject(FirebaseService);
+  nowDate = new Date;
+  currentHour = this.nowDate.getHours();
+  tasks: TaskInterface[] = [];
 
-nowDate = new Date;
-currentHour = this.nowDate.getHours();
-tasks: TaskInterface[] = [];
-
-todo: TaskInterface[] = [];
-inProgress: TaskInterface[] = [];
-awaitFeedback: TaskInterface[] = [];
-done: TaskInterface[] = [];
-filteredTasks: TaskInterface[] = [];
-urgent: TaskInterface[] = [];
+  todo: TaskInterface[] = [];
+  inProgress: TaskInterface[] = [];
+  awaitFeedback: TaskInterface[] = [];
+  done: TaskInterface[] = [];
+  filteredTasks: TaskInterface[] = [];
+  urgent: TaskInterface[] = [];
 
 
-constructor() {
-  this.firebase.tasksList$.subscribe((tasks: TaskInterface[]) => {
-    this.tasks = tasks;
-    this.filteredTasks = [...this.tasks]; // Anfangs alle Aufgaben anzeigen
-  });
-}
-
-sortTasks() {
-  this.todo = []; // Leere die Arrays vor dem erneuten Kategorisieren
-  this.awaitFeedback = [];
-  this.inProgress = [];
-  this.done = [];
-
-  this.filteredTasks.forEach((task) => {
-    this.categorizeTask(task);
-  });
-}
-
-getUrgentTaskWithLowestDate(): string {
-  this.sortTasksByPriority();
-  if (this.urgent.length === 0) {
-    return 'No urgent Tasks';
-  } else {
-    let taskWithLowestDate: TaskInterface = this.urgent[0];
-    this.urgent.forEach((urgentTask) => {
-      if (urgentTask.date) {
-        const currentDate = new Date(urgentTask.date);
-        const lowestDate = new Date(taskWithLowestDate.date || '');
-        if (currentDate < lowestDate) {
-          taskWithLowestDate = urgentTask;
-        }
-      }
+  constructor(private firebase: FirebaseService, private router: Router) {
+    this.firebase.tasksList$.subscribe((tasks: TaskInterface[]) => {
+      this.tasks = tasks;
+      this.filteredTasks = [...this.tasks]; // Anfangs alle Aufgaben anzeigen
     });
-    return this.transformUrgentTaskDate(taskWithLowestDate);
   }
-}
 
-transformUrgentTaskDate(task: TaskInterface ) {
-  if (task.date) {
-    const date = new Date(task.date);
-    const formattedDate = new Intl.DateTimeFormat('en-US', {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-    }).format(date);
-    return formattedDate;
-  } else {
-    return 'No date found';
+  sortTasks() {
+    this.todo = []; // Leere die Arrays vor dem erneuten Kategorisieren
+    this.awaitFeedback = [];
+    this.inProgress = [];
+    this.done = [];
+
+    this.filteredTasks.forEach((task) => {
+      this.categorizeTask(task);
+    });
   }
-}
 
-sortTasksByPriority() {
-  this.urgent = [];
-
-  this.filteredTasks.forEach((task) => {
-    this.categorizeTaskByPriority(task);
-  })
-}
-
-categorizeTaskByPriority(task: TaskInterface) {
-  if(task.priority === "urgent") {
-    this.urgent.push(task);
+  getUrgentTaskWithLowestDate(): string {
+    this.sortTasksByPriority();
+    if (this.urgent.length === 0) {
+      return 'No urgent Tasks';
+    } else {
+      let taskWithLowestDate: TaskInterface = this.urgent[0];
+      this.urgent.forEach((urgentTask) => {
+        if (urgentTask.date) {
+          const currentDate = new Date(urgentTask.date);
+          const lowestDate = new Date(taskWithLowestDate.date || '');
+          if (currentDate < lowestDate) {
+            taskWithLowestDate = urgentTask;
+          }
+        }
+      });
+      return this.transformUrgentTaskDate(taskWithLowestDate);
+    }
   }
-}
 
-categorizeTask(task: TaskInterface){
-  if(task.status === "todo") {
-    this.todo.push(task);
-  } else if(task.status === "awaitFeedback") {
-    this.awaitFeedback.push(task);
-  } else if(task.status === "inProgress") {
-    this.inProgress.push(task);
-  } else if(task.status === "done") {
-    this.done.push(task);
+  transformUrgentTaskDate(task: TaskInterface) {
+    if (task.date) {
+      const date = new Date(task.date);
+      const formattedDate = new Intl.DateTimeFormat('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+      }).format(date);
+      return formattedDate;
+    } else {
+      return 'No date found';
+    }
   }
-}
 
-getTodoTaskCount() {
-  this.sortTasks();
-  return this.todo.length;
-}
+  sortTasksByPriority() {
+    this.urgent = [];
 
-getAwaitFeedbackTaskCount() {
-  this.sortTasks();
-  return this.awaitFeedback.length;
-}
-
-getInProgressTaskCount() {
-  this.sortTasks();
-  return this.inProgress.length;
-}
-
-getDoneTasksCount() {
-  this.sortTasks();
-  return this.done.length;
-}
-
-getAllTaskCount() {
-  return this.filteredTasks.length;
-}
-
-getUrgentTaskCount() {
-  this.sortTasksByPriority();
-  return this.urgent.length;
-}
-
-
-//get different greetings for different day times
-getGreeting() {
-  if(this.currentHour > 3 && this.currentHour < 11) {
-    console.log(this.filteredTasks);
-    return 'Good morning';
+    this.filteredTasks.forEach((task) => {
+      this.categorizeTaskByPriority(task);
+    })
   }
-  else if(this.currentHour > 12 && this.currentHour < 16){
-    return 'Good afternoon';
+
+  categorizeTaskByPriority(task: TaskInterface) {
+    if (task.priority === "urgent") {
+      this.urgent.push(task);
+    }
   }
-  else if(this.currentHour > 17 && this.currentHour < 20){
-    return 'Good evening';
+
+  categorizeTask(task: TaskInterface) {
+    if (task.status === "todo") {
+      this.todo.push(task);
+    } else if (task.status === "awaitFeedback") {
+      this.awaitFeedback.push(task);
+    } else if (task.status === "inProgress") {
+      this.inProgress.push(task);
+    } else if (task.status === "done") {
+      this.done.push(task);
+    }
   }
-  else return 'Good night';
+
+  getTodoTaskCount() {
+    this.sortTasks();
+    return this.todo.length;
+  }
+
+  getAwaitFeedbackTaskCount() {
+    this.sortTasks();
+    return this.awaitFeedback.length;
+  }
+
+  getInProgressTaskCount() {
+    this.sortTasks();
+    return this.inProgress.length;
+  }
+
+  getDoneTasksCount() {
+    this.sortTasks();
+    return this.done.length;
+  }
+
+  getAllTaskCount() {
+    return this.filteredTasks.length;
+  }
+
+  getUrgentTaskCount() {
+    this.sortTasksByPriority();
+    return this.urgent.length;
+  }
+
+
+  //get different greetings for different day times
+  getGreeting() {
+    if (this.currentHour > 3 && this.currentHour < 11) {
+      console.log(this.filteredTasks);
+      return 'Good morning';
+    }
+    else if (this.currentHour > 12 && this.currentHour < 16) {
+      return 'Good afternoon';
+    }
+    else if (this.currentHour > 17 && this.currentHour < 20) {
+      return 'Good evening';
+    }
+    else return 'Good night';
+  }
+
+
+navigateToBoardView() {
+  this.router.navigate(['/board']);
 }
-
-
 
 
 
