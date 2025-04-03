781f5722a8f82e2b1e7b7b6415148efbab7b1796
Albina Januzi <albina_januzi@hotmail.com>
2025-04-03 15:00:03 +0200

    Heade Avatar initials when user logged in

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AppComponent } from '../../app.component';
+import { getAuth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +13,39 @@ import { AppComponent } from '../../app.component';
 })
 export class HeaderComponent {
   menuOpen = false;
+  userInitials: string = '';
+  isLoggedIn: boolean = false;
 
-  constructor(private appComponent: AppComponent) {}
+  constructor(private appComponent: AppComponent) {
+    this.fetchUserInitials();
+   }
+
+   // Fetch the initials of the logged-in user
+  private fetchUserInitials() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.isLoggedIn = true; // Benutzer ist angemeldet
+        const displayName = user.displayName || ''; // Verwende displayName, falls verfügbar
+        const email = user.email || ''; // Fallback auf E-Mail, falls displayName nicht gesetzt ist
+
+        if (displayName) {
+          const [firstName, lastName] = displayName.split(' ');
+          const firstInitial = firstName?.charAt(0).toUpperCase() || '';
+          const lastInitial = lastName?.charAt(0).toUpperCase() || '';
+          this.userInitials = `${firstInitial}${lastInitial}`;
+        } else if (email) {
+          const [firstPart] = email.split('@');
+          const firstInitial = firstPart.charAt(0).toUpperCase();
+          this.userInitials = `${firstInitial}`;
+        }
+      
+      }   else {
+        this.isLoggedIn = true; 
+        this.userInitials = 'G';
+      }
+    });
+  }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -23,14 +55,14 @@ export class HeaderComponent {
     this.appComponent.logout();
   }
 
-    // HostListener für Klicks überall auf der Seite
-    @HostListener('document:click', ['$event'])
-    closeMenuOnOutsideClick(event: Event) {
-      const targetElement = event.target as HTMLElement;
-      
-      // Überprüfen, ob das geklickte Element Teil des Three-Dot-Menüs oder des Dropdowns ist
-      if (!targetElement.closest('.menu-button') && !targetElement.closest('.dropdown_menu')) {
-        this.menuOpen = false;
-      }
+  // HostListener für Klicks überall auf der Seite
+  @HostListener('document:click', ['$event'])
+  closeMenuOnOutsideClick(event: Event) {
+    const targetElement = event.target as HTMLElement;
+
+    // Überprüfen, ob das geklickte Element Teil des Three-Dot-Menüs oder des Dropdowns ist
+    if (!targetElement.closest('.menu-button') && !targetElement.closest('.dropdown_menu')) {
+      this.menuOpen = false;
     }
+  }
 }
