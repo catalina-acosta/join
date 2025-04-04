744e4eb7dbac77adba6201f363c6cec3b2af3e83
Caryensong <caryensong@googlemail.com>
2025-04-04 09:17:02 +0200

    fix routerlink for privacy and legal notice

--- src/app/privacy-policy/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-
+ constructor(private router: Router) {
+   }
 }
