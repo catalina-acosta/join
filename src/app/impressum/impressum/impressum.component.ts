8381fe997cee68493980cf6d4dbcd2fdd2618087
Caryensong <caryensong@googlemail.com>
2025-04-04 09:17:02 +0200

    fix routerlink for privacy and legal notice

--- src/app/impressum/impressum/impressum.component.ts
+++ src/app/impressum/impressum/impressum.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-impressum',
-  imports: [RouterModule],
+  imports: [RouterModule, RouterLink],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
 export class ImpressumComponent {
-
+ constructor(private router: Router) {
+   }
 }
